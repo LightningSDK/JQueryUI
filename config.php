a179ea25e29d94309bb3ad37763048cd8fa85ff2
DAB <dab@dab.la>
2020-06-02 09:37:09 -0400

    initial commit

---
+++ config.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    'compiler' => [
+        'js' => [
+            'JQueryUI' => [
+                // Source file => Dest file
+                'vendor/components/jqueryui/jquery-ui.min.js' => 'admin.min.js',
+            ],
+        ],
+    ],
+];
